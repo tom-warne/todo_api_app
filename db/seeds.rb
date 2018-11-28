57e1646b26377fefc416187fde240cb2d0074377
Tom Warne <thomaswarne+github@gmail.com>
2018-11-28 14:04:14 -0800

    Create initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tag  = Tag.create(title:  'Today')
+task = Task.create(title: 'Wash laundry')
+
+TagTask.create(tag: tag, task: task)
