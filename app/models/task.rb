8e99fbbc1f94007f086faa4566459c61b3200a72
Tom Warne <thomaswarne+github@gmail.com>
2018-11-28 13:07:47 -0800

    Create Serializers

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   has_many :tag_tasks, dependent: :destroy
   has_many :tags,      through:   :tag_tasks
 
-  validate :title, presence: :true
+  validates :title, presence: true
 end
