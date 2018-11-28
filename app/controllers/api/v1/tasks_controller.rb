61d53d76371cd754e288eb0f8b3b152d2d2f3ff3
Tom Warne <thomaswarne+github@gmail.com>
2018-11-28 14:39:03 -0800

    Add controller queries and responses; Update AMS to take advantage of new strong parameter wrapper methods

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,15 +1,25 @@
 class API::V1::TasksController < API::V1::BaseController
 
   def create
+    render status: :ok, json: Task.create(task_params)
   end
 
   def destroy
+    render status: :ok, json: Task.destroy(task_parmas[:id])
   end
 
   def index
+    render status: :ok, json: Task.all
   end
 
   def update
+    render status: :ok, json: Task.find(task_params.delete(:id)).update(task_params)
   end
-  
+
+  private
+
+  def task_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse!(params, only: %i[id title tags])
+  end
+
 end
