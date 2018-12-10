7f8812763214f9f966214478aa1154ee2acee4a2
Tom <thomaswarne+github@gmail.com>
2018-12-09 23:12:32 -0800

    Protect ar_internal_metadata from DatabaseCleaner.  Prevents `ActiveRecord::NoEnvironmentInSchemaError` after running the test suite

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,4 +1,4 @@
 require 'database_cleaner'
 
 DatabaseCleaner.strategy = :transaction
-DatabaseCleaner.clean_with(:truncation)
+DatabaseCleaner.clean_with(:truncation, except: %w(ar_internal_metadata))
